58a73914320e739a7ca874218bc2812706214e48
«jordi» <jordivega@iesebre.com>
2024-12-18 19:30:39 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\ComptadorController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/comptador', [ComptadorController::class, 'index']);
+Route::post('/incrementar', [ComptadorController::class, 'incrementar']);
+Route::post('/decrementar', [ComptadorController::class, 'decrementar']);
+Route::post('/reset', [ComptadorController::class, 'reset']);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
